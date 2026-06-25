c43882738311b4900d20a5c5ecd2ecd707e5f9da
chethan <119585318+chethanarunkumar@users.noreply.github.com>
2026-06-25 12:41:23 +0530

    Update test_whatsapp.py

--- NextGen/test_whatsapp.py
+++ NextGen/test_whatsapp.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-ACCESS_TOKEN ="EAAJsOhd98kgBQYgnESFhQcQLSof8FH7V2w1WrlFPQzNaxfwsdNU6JWU3n2kGUsOM1rot6vhc48WeZCFJGweUKtXRjAr0wONZAtWCZAQcZCXZAtNnHJConQjGPlSjaftzsds8ZAljc8ZAkDJP7cJqdE7VPzKO4ddFmTW1lFzemgWAkUBZCL6bbAzNZCsXJ9o2nmA4sCbl82ElQXTMOrT4XiOW6Gab09ZBTfTZB2oUG70429b14wcosSPu8aS7jwsVKTA7lZBqFZA5lrGnVEECbrBGZBXvA6hrP6"
+ACCESS_TOKEN =" "
 
 # 🔥 ADD THIS LINE (YOUR PHONE NUMBER ID)
-PHONE_NUMBER_ID = "886425391222264"
+PHONE_NUMBER_ID = " "
 
 def send_whatsapp_message(number, message):
     url = f"https://graph.facebook.com/v18.0/{PHONE_NUMBER_ID}/messages"
@@ -25,6 +25,6 @@ def send_whatsapp_message(number, message):
     print(response.json())
 
 
-num = "+919606641407"   # your verified test number
+num = "+91 "   # your verified test number
 message = "Hello! This is a test from NGIM  from msruas🔔"
 send_whatsapp_message(num, message)
